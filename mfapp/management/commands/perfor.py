9c3fee9caf96434de923b1834a459d8327f5a11a
Nitesh Jitendra Mishra <115858622+Nitesh13875@users.noreply.github.com>
2024-10-31 12:21:16 +0530

    Update perfor.py

--- mfapp/management/commands/perfor.py
+++ mfapp/management/commands/perfor.py
@@ -47,28 +47,28 @@ class Command(BaseCommand):
         except requests.exceptions.RequestException as e:
             self.stdout.write(self.style.ERROR(f"Error fetching data: {e}"))
             return None
+            
+def fetch_fund_data(self, ids):
+    for fund_id in ids:
+        url = f"https://api-global.morningstar.com/sal-service/v1/fund/quote/v4/{fund_id}/data?fundServCode=&showAnalystRatingChinaFund=false&showAnalystRating=false&languageId=en&locale=en&clientId=RSIN_SAL&benchmarkId=mstarorcat&component=sal-mip-quote&version=4.13.0&access_token={self.ACCESS_TOKEN}"
+        response = self.fetch_data(url)
+        if not response:
+            continue
 
-    def fetch_fund_data(self, ids, access_token):
-        for fund_id in ids:
-            url = f"https://api-global.morningstar.com/sal-service/v1/fund/quote/v4/{fund_id}/data?fundServCode=&showAnalystRatingChinaFund=false&showAnalystRating=false&languageId=en&locale=en&clientId=RSIN_SAL&benchmarkId=mstarorcat&component=sal-mip-quote&version=4.13.0&access_token={access_token}"
-            response = self.fetch_data(url)
-            if not response:
-                continue
+        fund_data = response.json()
+        sec_id = fund_data.get("secId")
+        fund_defaults = {
+            "isin": fund_data.get("isin"),
+            "investment_name": fund_data.get("investmentName"),
+            "inceptionDate": pd.to_datetime(fund_data.get("inceptionDate")).date() if fund_data.get("inceptionDate") else None,
+            "prospectus_benchmark_name": fund_data.get("prospectusBenchmarkName"),
+            "expense_ratio": float(fund_data.get("expenseRatio")) if fund_data.get("expenseRatio") not in [None, 'NA', ''] else None,
+            "last_turnover_ratio": float(fund_data.get("lastTurnoverRatio")) if fund_data.get("lastTurnoverRatio") not in [None, 'NA', ''] else None,
+            "equity_style_box": fund_data.get("equityStyleBox"),
+            "expense": float(fund_data.get("expense")) if fund_data.get("expense") not in [None, 'NA', ''] else None,
+            "morningstar_rating": int(fund_data.get("morningstarRating")) if fund_data.get("morningstarRating") not in [None, 'NA', ''] else None,
+            "total_asset": float(fund_data.get("totalAsset")) if fund_data.get("totalAsset") not in [None, 'NA', ''] else None,
+        }
 
-            fund_data = response.json()
-            sec_id = fund_data.get("secId")
-            fund_defaults = {
-                "isin": fund_data.get("isin"),
-                "investment_name": fund_data.get("investmentName"),
-                "inceptionDate": pd.to_datetime(fund_data.get("inceptionDate")).date() if fund_data.get("inceptionDate") else None,
-                "prospectus_benchmark_name": fund_data.get("prospectusBenchmarkName"),
-                "expense_ratio": float(fund_data.get("expenseRatio")) if fund_data.get("expenseRatio") not in [None, 'NA', ''] else None,
-                "last_turnover_ratio": float(fund_data.get("lastTurnoverRatio")) if fund_data.get("lastTurnoverRatio") not in [None, 'NA', ''] else None,
-                "equity_style_box": fund_data.get("equityStyleBox"),
-                "expense": float(fund_data.get("expense")) if fund_data.get("expense") not in [None, 'NA', ''] else None,
-                "morningstar_rating": int(fund_data.get("morningstarRating")) if fund_data.get("morningstarRating") not in [None, 'NA', ''] else None,
-                "total_asset": float(fund_data.get("totalAsset")) if fund_data.get("totalAsset") not in [None, 'NA', ''] else None,
-            }
-
-            Fund.objects.update_or_create(sec_id=sec_id, defaults=fund_defaults)
-            self.stdout.write(self.style.SUCCESS(f"Stored data for Fund ID: {fund_id}"))
+        Fund.objects.update_or_create(sec_id=sec_id, defaults=fund_defaults)
+        self.stdout.write(self.style.SUCCESS(f"Stored data for Fund ID: {fund_id}"))
